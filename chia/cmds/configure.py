93b1fab1dbaad14ea2b55e45dc3aa2ff1df88204
huangxufu <huangxufu2017@gmail.com>
2021-06-22 16:36:49 +0800

    update default config

--- chia/cmds/configure.py
+++ chia/cmds/configure.py
@@ -77,7 +77,7 @@ def configure(
             print("Setting Testnet")
             testnet_port = "58444"
             testnet_introducer = "beta1_introducer.chia.net"
-            testnet = "testnet7"
+            testnet = "testnet8"
             config["full_node"]["port"] = int(testnet_port)
             config["full_node"]["introducer_peer"]["port"] = int(testnet_port)
             config["farmer"]["full_node_peer"]["port"] = int(testnet_port)
