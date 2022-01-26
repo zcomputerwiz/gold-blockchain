6bb76a36cc824f5f413e2918882a25abdd3dc944
Waruta <62998111+WarutaShinken@users.noreply.github.com>
2022-01-27 01:18:33 +1100

    Renamed Environment Variables
    CHIA => SIT

--- tests/block_tools.py
+++ tests/block_tools.py
@@ -1296,7 +1296,7 @@ def get_challenges(
 
 
 def get_plot_dir() -> Path:
-    cache_path = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/"))) / "test-plots"
+    cache_path = Path(os.path.expanduser(os.getenv("SIT_ROOT", "~/.chia/"))) / "test-plots"
     mkdir(cache_path)
     return cache_path
 
